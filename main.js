0bb9f5c7985c73fc73b0c2c53ac4de82d01a94c6
kirtirajambadaskadam <86154528+kirtirajambadaskadam@users.noreply.github.com>
2021-12-15 19:07:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+function setup(){
+    canvas=createCanvas(600,500)
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotposes);
+}
+
+function gotposes(results){
+ if(results.length > 0){
+     console.log(results);
+     scoreLeftWrist= results[0].pose.keypoints[9].score;
+     console.log("scoreLeftWrist = "+scoreLeftWrist);
+
+     leftWristX= results[0].pose.leftWrist.x;
+     leftWristY= results[0].pose.leftWrist.y;
+     console.log("leftWristX ="+ leftWristX + "leftWristY =" + leftWristY);
+
+     rightWristX=results[0].pose.rightWrist.x;
+     rightWristY=results[0].pose.rightWrist.y;
+     console.log("rightWristX ="+rightWristX+ "rightWristY ="+rightWristY);
+ }
+}
+
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
+scoreLeftWrist=0;
+scoreRightWrist=0;
+
+song1="";
+song2="";
+song1_status="";
+song2_status="";
+
+function draw(){
+    image(video,0,0,600,500);
+
+    song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+	fill("#FF0000");
+	stroke("#FF0000");
+
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+
+			song1.stop();
+
+		if(song2_status == false)
+		{
+			song2.play();
+			document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song"
+		}
+	}
+ 
+
+  
+}
+
+
+function preload(){
+    song1= loadSound("Stay(PagalWorld).mp3");
+    song2= loadSound("music.mp3")
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+
+
+function play(){
+    song.play();
+	song.setVolume(1);
+	song.rate(1);
+}
